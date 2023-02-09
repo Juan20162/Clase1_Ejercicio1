b5b45ce5b0bd833e761bf398d237a77a94ea5645
ACER NITRO 5 <ACER NITRO 5@DESKTOP-P986EIO>
2023-02-08 19:04:47 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio1.cs
@@ -0,0 +1,11 @@
+﻿/*Ejercicio 1
+Escribe un programa que reciba tu nombre y lo escriba por consola. */
+
+Console.WriteLine("Introduce tu nombre: ");
+string name = Console.ReadLine();
+Console.WriteLine("Hola, " + name);
+
+
+
+
+
